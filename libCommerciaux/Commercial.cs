58a7d0c42e22824a6018e874482aa89032c8f835
z <z@DESKTOP-F1BS06T>
2025-11-14 13:57:22 +0100

    Commerciaux

--- libCommerciaux/Commercial.cs
+++ libCommerciaux/Commercial.cs
@@ -1,7 +1,59 @@
-﻿namespace libCommerciaux
+﻿using System;
+using System.Collections.Generic;
+
+namespace libCommerciaux
 {
     public class Commercial
     {
-        
+  
+        private string nom;
+        private string prenom;
+        private int puissanceVoiture;
+        private char categorie;
+        private List<NoteFrais> mesNotes;
+
+        public Commercial(string nom, string prenom, int puissanceVoiture, char categorie)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.puissanceVoiture = puissanceVoiture;
+            this.categorie = categorie;
+            this.mesNotes = new List<NoteFrais>();
+        }
+
+        public string getNom()
+        {
+            return nom;
+        }
+
+        public string getPrenom()
+        {
+            return prenom;
+        }
+
+        public int getPuissanceVoiture()
+        {
+            return puissanceVoiture;
+        }
+
+        public char getCategorie()
+        {
+            return categorie;
+        }
+
+        public List<NoteFrais> getMesNotes()
+        {
+            return mesNotes;
+        }
+
+        public void ajouterNoteFrais(NoteFrais note)
+        {
+            mesNotes.Add(note);
+        }
+
+        public override string ToString()
+        {
+            return $"Nom : {nom}, Prénom : {prenom}, Catégorie : {categorie}, Voiture : {puissanceVoiture} CV, Nombre de notes : {mesNotes.Count}";
+        }
     }
 }
---
+++ libCommerciaux/NoteFrais.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace libCommerciaux
+{
+    public class NoteFrais
+    {
+        // Champs privés (les tests vont les chercher avec reflection)
+        private int numero;
+        private DateTime dateNoteFrais;
+        private double montantARembourser;
+        private bool estRembourse;
+        private Commercial leCommercial;
+
+        public NoteFrais(DateTime dateNoteFrais, Commercial leCommercial)
+        {
+            this.numero = 0; 
+            this.dateNoteFrais = dateNoteFrais;
+            this.leCommercial = leCommercial;
+            this.estRembourse = false;
+            this.montantARembourser = 0.0;
+        }
+
+        public double getMontantARembourser()
+        {
+            return montantARembourser;
+        }
+
+        public void setMontantARembourser(double montant)
+        {
+            this.montantARembourser = montant;
+        }
+
+        public Commercial getLeCommercial()
+        {
+            return leCommercial;
+        }
+
+        public bool getEstRembourse()
+        {
+            return estRembourse;
+        }
+
+        public void setRembourse()
+        {
+            this.estRembourse = true;
+        }
+
+        public virtual double calculMontantARembourser()
+        {
+            return montantARembourser;
+        }
+
+        public override string ToString()
+        {
+            string remboursement = estRembourse ? "Remboursé" : "Non remboursé";
+            return $"Numéro : {numero}, Date : {dateNoteFrais:dd/MM/yyyy}, Montant à rembourser: {montantARembourser}€, {remboursement}";
+        }
+    }
+}
